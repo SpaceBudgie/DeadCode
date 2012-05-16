8a14a74b1837158dcf61d2cc290a550e335ec3dd
Bullet Magnet Games <philcooperking@gmail.com>
2012-05-17 00:15:17 +0100

    Added files for events, And Unit Testing.

---
+++ Dead/Events/SimpleEventManager.hpp
@@ -0,0 +1,24 @@
+
+
+#ifndef DEAD_SIMPLE_EVENT_MANAGER_INCLUDED
+#define DEAD_SIMPLE_EVENT_MANAGER_INCLUDED
+
+namespace Dead {
+
+
+struct SimpleQueue
+{
+
+}; // struct
+
+
+
+template<typename Controller, typename EventID, typename Event, typename EventQueue = SimpleQueue>
+class SimpleEventManager
+{
+
+}; // class
+
+}  // namespace
+
+#endif // include guard
